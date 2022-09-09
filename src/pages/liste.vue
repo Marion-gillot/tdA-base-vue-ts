09fd48a3f41d4802378251b57a6da86e0f5e45e4
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2022-09-09 15:00:41 +0200

    import de la card et v-bind

---
+++ src/pages/liste.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Card from "../components/card.vue";
+
+
+    
+
+
+    
+</script>
+    
+
+<template>
+</template>
+
+
+<style scoped>
+</style>
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,17 +1,20 @@
-<script setup>
+<script setup lang="ts">
+import Card from "../components/card.vue";
+const maison0 = {
+        name:'maison 1',
+        prix:'4300€',
 
-// Import du composant 
-import card from '@/components/card.vue';
-
+    };
 </script>
 
 
-
-
-
-
 <template>
-    
+    <h4 class="mt-8 mb-8 font-bold text-center">Offre Maison</h4>
+    <Card :prix="50"/>
+
+    <Card  v-bind="maison0"/>
+
+
 </template>
 
 
