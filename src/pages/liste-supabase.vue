1d7dc17ea1c424d84ee6cd541d2b84934238d97b
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2022-09-23 10:11:48 +0200

    page liste-supabase.vue

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const maisons = []; // à remplacer par l'appel à Supabase
+</script>
+
+<template>
+<div class="p-2">
+    <h1 class="text-2xl">Page Liste</h1>
+    <FicheOffreMaison v-for="maison in maisons" :key="maison.nom" v-bind="maison" />
+    <card class="w-1/2" v-bind="maisons"/>
+</div>
+</template>
+
+
